a5ec2fdc5806a56bb9d3f637a144c42c4b2c2603
Eric Jacobson <contact@efjacobson.com>
2016-09-02 22:23:56 -0700

    Buddy Domain

---
+++ best-vethack3/Models/Domain/Buddy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace best_vethack3.Models.Domain
+{
+    public class Buddy
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int IsActive { get; set; }
+        public string Branch { get; set; }
+        public string Rank { get; set; }
+        public int YearsServed { get; set; }
+        public string Location { get; set; }
+        public string CurrentOccupation { get; set; }
+        public string TagLine { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ best-vethack3/Services/BuddyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace best_vethack3.Services
+{
+    public class BuddyService
+    {
+    }
+}
