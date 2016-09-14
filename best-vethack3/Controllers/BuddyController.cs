ea26db94a8a5a9a40a26086d09e9bee212bf8ca6
Eric Jacobson <contact@efjacobson.com>
2016-09-13 22:20:35 -0700

    consolidate

---
+++ best-vethack3/Controllers/BuddyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace best_vethack3.Controllers
+{
+    public class BuddyController : Controller
+    {
+        public ActionResult Find()
+        {
+            return View();
+        }
+    }
+}
